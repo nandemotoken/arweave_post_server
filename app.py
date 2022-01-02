d1050d768b684f5304b39278a926462c78a92a65
nandemotoken <nandemotoken@gmail.com>
2022-01-02 18:29:45 +0000

    add

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def arpost():
         #filepath = f"/tmp/{key}"
         #file.save(filepath)
         #returnstr.append(str(filepath))
-        returnstr = returnstr + str(file.read())
+        returnstr = returnstr + str(file)
     return jsonify({"returnstr":returnstr})
 
 
